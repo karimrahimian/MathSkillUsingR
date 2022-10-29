5b1382f15639a80a01837bf175447ed27c8bc891
karim rahimian <karim.rahimian@gmail.com>
2022-10-30 01:30:54 +0330

    OncoPlot

--- Homework2/potplot.R
+++ Homework2/potplot.R
@@ -4,13 +4,13 @@ getOrderedUniqueHugoNames<-function(hugoNames){
   uniqueHugoNames = data.frame(table(hugoNames))
   filteredHugoNamesIndex= which(uniqueHugoNames$Freq>=5)
   filteredUniqueHugoNames = uniqueHugoNames[filteredHugoNamesIndex,]
-  orderedUniqueHugoName = filteredUniqueHugoNames[order(filteredUniqueHugoNames$Freq,decreasing = FALSE),]
+  orderedUniqueHugoName = filteredUniqueHugoNames[order(filteredUniqueHugoNames$Freq,decreasing = TRUE),]
   return (orderedUniqueHugoName)
 }
 
 getOrderedUniqueSamples<-function(samples){
   uniqueSamples = data.frame(table(samples))
-  filteredUniqueSamplesIndex= which(uniqueSamples$Freq>=2)
+  filteredUniqueSamplesIndex= which(uniqueSamples$Freq>=1)
   filteredSample = uniqueSamples[filteredUniqueSamplesIndex,]
   orderedUniqueSample = filteredSample[order(filteredSample$Freq,decreasing = FALSE),]
   return(orderedUniqueSample)
@@ -36,7 +36,8 @@ calcSum <-function(vector){
 drawRightBoxPlot<-function(row,col,vector,maxdata){
   sum = calcSum(vector)
   nextStep = col
-  colors = c('blue','beige','orange','green','red','black','yellow')
+
+  colors = c('blue','purple','orange','green','red','black','yellow')
   for (k in 1:length(vector)){
     perc = ((vector[k] / maxdata))*20
     
@@ -52,7 +53,7 @@ drawRightBoxPlot<-function(row,col,vector,maxdata){
 drawTopBoxPlot<-function(row,col,vector,maxdata){
   sum = calcSum(vector)
   nextStep = row
-  colors = c('blue','beige','orange','green','red','black','yellow')
+  colors = c('blue','purple','orange','green','red','black','yellow')
   for (k in 1:length(vector)){
     perc = ((vector[k] / maxdata))*3
 
@@ -63,12 +64,14 @@ drawTopBoxPlot<-function(row,col,vector,maxdata){
             col = colors[k],
             border = NA)
   }
+  
 }
 
 drawButtonBoxPlot<-function(row,col,vector){
   sum = calcSum(vector)
   nextStep = row
-  colors = c('blue','beige','orange','green','red','black','yellow')
+  #colors = c('blue','purple','orange','green','red','black','yellow')
+  colors = c('pink','orange1','cyan')
   for (k in 1:length(vector)){
     perc = ((vector[k] / sum))*4
     yCoords = c(nextStep, nextStep+perc, nextStep+perc, nextStep)
@@ -78,6 +81,8 @@ drawButtonBoxPlot<-function(row,col,vector){
             col = colors[k],
             border = NA)
   }
+
+  
 }
 
 putLegents<-function(x,y,legends,colors){
@@ -93,7 +98,31 @@ putLegents<-function(x,y,legends,colors){
   }
 }
 
-drawHeatmap<-function (heatData,xLabel,yLabel,rightBar,topBar,variants){
+putButtomLegend<-function(xStart,y){
+  xStart = xStart + 3
+
+  xCoords = c(xStart+0.1,xStart+3-0.1,xStart+3-0.1,xStart+0.1)
+  yCoords = c(y-0.1,y-0.1,y+1+0.1,y+1+0.1)
+  
+  polygon(xCoords,yCoords,col = 'pink',border = NA)
+  text(xStart+10,y+0.5,"C>A",cex = 0.4)
+  
+  y=-2.1
+  xCoords = c(xStart+0.1,xStart+3-0.1,xStart+3-0.1,xStart+0.1)
+  yCoords = c(y-0.1,y-0.1,y+1+0.1,y+1+0.1)
+  
+  polygon(xCoords,yCoords,col = 'orange1',border = NA)
+  text(xStart+10,y+0.5,"C>T",cex = 0.4)
+
+  y=-3.2
+  xCoords = c(xStart+0.1,xStart+3-0.1,xStart+3-0.1,xStart+0.1)
+  yCoords = c(y-0.1,y-0.1,y+1+0.1,y+1+0.1)
+  
+  polygon(xCoords,yCoords,col = 'cyan',border = NA)
+  text(xStart+10,y+0.5,"C>G",cex = 0.4)
+}
+
+drawHeatmap<-function (heatData,xLabel,yLabel,rightBar,topBar,buttomBar,variants){
   sampleSize = ncol(heatData)
   xSize <- ncol(heatData)
   ySize <- nrow(heatData)
@@ -101,9 +130,9 @@ drawHeatmap<-function (heatData,xLabel,yLabel,rightBar,topBar,variants){
   plot (c(-10,xSize+40),
         c(-30,ySize+20),
         type= 'n',
-        axes = TRUE, 
+        axes = FALSE, 
         ann=FALSE)
-  colors = c('gray','blue','beige','orange','green','red','black','yellow')
+  colors = c('gray','blue','purple','orange','green','red','black','yellow')
   # loops for create heatmap using polygon statement
   for(i in (ySize+1):2){
     for (j in 1:xSize){
@@ -119,22 +148,60 @@ drawHeatmap<-function (heatData,xLabel,yLabel,rightBar,topBar,variants){
     text(j+5,i-0.5,paste(round(percentageValue*100),"%"),cex = 0.4)
     drawRightBoxPlot(i,j+10,rightBar[i-1,],max(rightBar))
   }
-    
+  
+  xCoords = c(xSize+10,xSize+10,xSize+40,xSize+40)
+  yCoords = c(ySize+3,ySize+2,ySize+2,ySize+3)
+  lines(xCoords,yCoords,type='l')
+  text(xSize+25,ySize+3,max(rightBar),cex=0.4)
 
   for (j in 1:xSize){
     drawTopBoxPlot(ySize+2,j+1,topBar[j,],max(topBar))
-    drawButtonBoxPlot(-3,j+1,topBar[j,])
+    drawButtonBoxPlot(-3,j+1,buttomBar[j,])
   }
+  putButtomLegend(xSize,-1)
   putLegents(0,-5,variants,colors)
-  
 }
 
+getFirstZeroColumn<-function(array,colStart){
+  len = length(array)
+  for (i in 1: len){
+    if (i>=colStart && array[i]==0){
+      return (i)
+    }
+  }
+  return (-1)
+}
 
+sortMatrix <-function(dataMatrix){
+    sortedMatrix = dataMatrix
+    i=1
+    colSize = ncol(sortedMatrix)
+    startColumn = 1
+    zeroColumn = 0
+    stackedIndex = c()
 
-  
+    while ( i<(nrow(sortedMatrix)) ){
+      if (i==1){
+        sortedIndex = order(sortedMatrix[i,1:colSize],decreasing = TRUE)
+        sortedMatrix = sortedMatrix[,sortedIndex]
+        zeroColumn = getFirstZeroColumn(sortedMatrix[i,],1)
+        stackedIndex = c(stackedIndex,1:(zeroColumn-1))
+      }
+      else
+      {
+        sortedIndex = order(sortedMatrix[i,zeroColumn:colSize],decreasing = TRUE) + (zeroColumn-1)
+        tempIndex = c(stackedIndex,sortedIndex)
+        sortedMatrix = sortedMatrix[,tempIndex]
+        zeroColumn = getFirstZeroColumn(sortedMatrix[i,],zeroColumn)
+        stackedIndex = c(1:zeroColumn-1)
+      }    
+      #print (i)
+      i=i+1
+    }
+    return (sortedMatrix)
+}
 
-
-oncoplot12<-function(lamf){
+MyOncoplot<-function(lamf){
   lamfData = lamf@data
   hugoSymbols = getOrderedUniqueHugoNames(lamfData$Hugo_Symbol)
   samples = getOrderedUniqueSamples(lamfData$Tumor_Sample_Barcode)
@@ -147,25 +214,65 @@ oncoplot12<-function(lamf){
   dataMatrix = matrix(0,rowSize,colSize)
   rightBar = matrix(0,rowSize,variantSize)
   topBar = matrix(0,colSize,variantSize)
-  buttonBar =matrix(0,colSize,variantSize)
+  buttonBar =matrix(0,colSize,3)
   
   lamfDataSize = nrow(lamfData)
+  gene_count =0
+  
   for (i in 1:lamfDataSize){
-    row = lamfData[i,]
-    index_hugo = which(hugoSymbols$hugoNames == row$Hugo_Symbol)
-    index_sample = which(samples$samples == row$Tumor_Sample_Barcode)
-    index_variant = which(variants$variant == row$Variant_Classification)
+    myRow = lamfData[i,]
+    index_hugo = which(hugoSymbols$hugoNames == myRow$Hugo_Symbol)
+    index_sample = which(samples$samples == myRow$Tumor_Sample_Barcode)
+    index_variant = which(variants$variant == myRow$Variant_Classification)
+    tumor_sample_Alelle = myRow$Tumor_Seq_Allele2
+    referenceAllele = myRow$Reference_Allele
     
-    if (length(index_hugo)>0){
+    if (length(index_hugo)>0 && length(index_sample)>0  && length(index_variant)>0){
+      if (dataMatrix[index_hugo,index_sample]!=0){
+        if (index_hugo==1){
+          print (paste("duplicate",index_sample,"_",index_variant))
+        }
+      }
+        
       dataMatrix [index_hugo,index_sample] = index_variant
-      rightBar[index_hugo,index_variant] = rightBar[index_hugo,index_variant] + 1 
-      topBar[index_sample,index_variant] = topBar[index_sample,index_variant] + 1 
-      buttonBar[index_sample,index_variant] = buttonBar[index_sample,index_variant] + 1 
+      if (tumor_sample_Alelle=="C"){
+        if (referenceAllele == "T"){
+          buttonBar[index_sample,1] = buttonBar[index_sample,1]+1
+        }else if (referenceAllele == "G"){
+          buttonBar[index_sample,2] = buttonBar[index_sample,2]+1
+        }else if (referenceAllele == "A"){
+          buttonBar[index_sample,3] = buttonBar[index_sample,3]+1
+        }
+      }
     }    
-    
   }
+  
+  print(paste("Gene Count",gene_count))
+  sumOfRows = rowSums(dataMatrix>0)
+  sortedIndex = order(sumOfRows,decreasing = TRUE)
+
+  dataMatrix = dataMatrix[sortedIndex,]
+  newSum = rowSums(dataMatrix>0)
+  
+  dataMatrix = sortMatrix(dataMatrix)
+  zeroColumnIndex = which(colSums(dataMatrix) != 0 )
+  dataMatrix = dataMatrix[, zeroColumnIndex ]
+  topBar = topBar[zeroColumnIndex,]
+  buttonBar = buttonBar[zeroColumnIndex,]
+  rowSize = nrow(dataMatrix)
+  colSize = ncol(dataMatrix)
+
+  for (i in 1:rowSize){
+    for (j in 1:colSize){
+      index_variant = dataMatrix[i,j]
+      rightBar[i,index_variant] = rightBar[i,index_variant] + 1 
+      topBar[j,index_variant] = topBar[j,index_variant] + 1 
+    }
+  }
+  
+  
   return (list("data"=dataMatrix, 
-               "yLabel" = c(hugoSymbols$hugoNames),
+               "yLabel" = c(hugoSymbols$hugoNames[sortedIndex]),
                "rightBar"= rightBar,
                "xLabel" = c(samples$samples),
                "topBar" = topBar,
@@ -183,12 +290,13 @@ laml = read.maf(maf = laml.maf,
                 verbose = FALSE)
 #head(laml@data)
 #oncoplot(laml@data)
-result = oncoplot12(laml)
+result = MyOncoplot(laml)
 pdf('result.pdf')
 drawHeatmap(result$data, 
             result$xLabel,
             result$yLabel,
             result$rightBar,
             result$topBar,
+            result$buttonBar,
             result$variants)
 dev.off()
